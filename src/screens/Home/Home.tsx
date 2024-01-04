6de5cf27b6d66aa5e8e1842d4598cc5803121344
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2024-01-03 21:15:17 -0300

    feat(addParticipante) - Criado componente para adicionar aprticipante

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,11 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 
 export default function Home() {
+  const handleParticipantsAdd = () => {
+    console.log('Você adicionou um participante!');
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Quarta, 3 de Janeiro de 2024</Text>
+      <TextInput
+        placeholder='Nome do participant'
+        placeholderTextColor='#6B6B6B'
+        style={styles.input}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantsAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,10 +1,26 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#31CF67',
+    borderRadius: 5,
+    justifyContent: 'center',
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 24,
+  },
   container: {
     backgroundColor: '#131016',
     flex: 1,
     padding: 24,
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16,
   },
   eventName: {
     color: '#FDFCFE',
@@ -12,8 +28,12 @@ export const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: 48,
   },
-  eventDate: {
-    color: '#6B6B6B',
+  input: {
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    color: '#FDFCFE',
     fontSize: 16,
+    height: 56,
+    padding: 16,
   },
 });
